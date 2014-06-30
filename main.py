3dee33570ae550634b587c07d8cc98048c45aba6
Johan van Litsenborgh <johan.vanLitsenborgh@bcx.co.za>
2014-06-30 14:51:32 +0200

    Added all routes to go to timeline and only / to go to the home page.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import webapp2
 import cgi
 
+#import urllib
+
+# import xml.etree.ElementTree as ET
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.write(open('index.html', 'r').read())
 
+class Timeline(webapp2.RequestHandler):
+    def get(self, twitter_handle):
+        url = 'http://twitter.com/' + twitter_handle
+        html = urllib.urlopen(url).read()
+        self.response.write(html)
+
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+  ('/', MainPage),
+  ('/(\w+)', Timeline)
 ], debug=True)
 
 def main():
-    from paste import httpserver
-    httpserver.serve(application, host='127.0.0.1', port='8080')
+  from paste import httpserver
+  httpserver.serve(application, host='127.0.0.1', port='8080')
 
 if __name__ == '__main__':
-    main()
+  main()
